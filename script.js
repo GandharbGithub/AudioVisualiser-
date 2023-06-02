62f11a9e8805cb20c422d7ff00cdc6c76729f24c
Sahoo Aditya Gandharb <119097482+GandharbGithub@users.noreply.github.com>
2023-06-03 01:57:13 +0530

    finally! done with basic white graphs dancing.

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
-const play = document.getElementById('play');
-const pause = document.getElementById('pause');
+const container = document.getElementById('container');
+const canvas = document.getElementById('canvas');
+const file = document.getElementById('fileupload');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
+let audioSource;
+let analyser;
 
-let audio1 = new Audio();
-audio1.src = 'DatPhoria - Scream It Loud (320K).mp3';
+file.addEventListener('change', function () {
+    // console.log(this.files);
+    const files = this.files;
+    const audio = document.getElementById('audio');
+    const audioContext = new AudioContext();
+    audio.src = URL.createObjectURL(files[0]);
+    audio.load();
+    audio.play();
 
-play.addEventListener('click', () => {
-    audio1.play();
-})
+    audioSource = audioContext.createMediaElementSource(audio);
+    analyser = audioContext.createAnalyser();
+    audioSource.connect(analyser);
+    analyser.connect(audioContext.destination);
+    analyser.fftSize = 64;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
 
-pause.addEventListener('click', () => {
-    audio1.pause();
-})
+    const barWidth = canvas.width / bufferLength;
+    let barHeight;
+    let x;
+
+    function animate() {
+        x = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        analyser.getByteFrequencyData(dataArray);
+        for (let i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i]*1.5;
+            ctx.fillStyle = 'white';
+            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+            x += barWidth;
+        }
+        requestAnimationFrame(animate);
+    } animate();
+});
