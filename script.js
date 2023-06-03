e9dfaeae1df78cfbac99eaeb670c9e6da8fa2bd5
Sahoo Aditya Gandharb <119097482+GandharbGithub@users.noreply.github.com>
2023-06-03 16:30:34 +0530

    colors

--- script.js
+++ script.js
@@ -6,38 +6,39 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 let audioSource;
 let analyser;
-
 file.addEventListener('change', function () {
-    // console.log(this.files);
     const files = this.files;
     const audio = document.getElementById('audio');
     const audioContext = new AudioContext();
     audio.src = URL.createObjectURL(files[0]);
     audio.load();
     audio.play();
-
     audioSource = audioContext.createMediaElementSource(audio);
     analyser = audioContext.createAnalyser();
     audioSource.connect(analyser);
     analyser.connect(audioContext.destination);
-    analyser.fftSize = 64;
+    analyser.fftSize = 512;
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
-
     const barWidth = canvas.width / bufferLength;
     let barHeight;
     let x;
-
     function animate() {
         x = 0;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         analyser.getByteFrequencyData(dataArray);
-        for (let i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i]*1.5;
-            ctx.fillStyle = 'white';
-            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-            x += barWidth;
-        }
+        drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray)
         requestAnimationFrame(animate);
     } animate();
 });
+function drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray) {
+    for (let i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i]*1.4;
+        const red = barHeight/2;
+        const green = i * barHeight;
+        const blue = barHeight/1.2;
+        ctx.fillStyle = 'rgb(' + red + ',' + green + ',' + blue + ')';
+        ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+        x += barWidth;
+    }
+}
