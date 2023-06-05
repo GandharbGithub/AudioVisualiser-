543bd89d9d683019ca9c0fe5fa9d7d047d21a665
Sahoo Aditya Gandharb <119097482+GandharbGithub@users.noreply.github.com>
2023-06-06 00:20:20 +0530

    refresh

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ file.addEventListener('change', function () {
     analyser = audioContext.createAnalyser();
     audioSource.connect(analyser);
     analyser.connect(audioContext.destination);
-    analyser.fftSize = 1024;
+    analyser.fftSize = 512;
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
     const barWidth = canvas.width / bufferLength;
