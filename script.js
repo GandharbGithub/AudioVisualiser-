813a51d96a783436927df58bcfe650646147ed0a
Sahoo Aditya Gandharb <119097482+GandharbGithub@users.noreply.github.com>
2023-06-05 11:28:52 +0530

    new

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ file.addEventListener('change', function () {
     analyser = audioContext.createAnalyser();
     audioSource.connect(analyser);
     analyser.connect(audioContext.destination);
-    analyser.fftSize = 2048;
+    analyser.fftSize = 1024;
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
     const barWidth = canvas.width / bufferLength;
@@ -36,7 +36,7 @@ function drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray) {
         barHeight = dataArray[i] * 1.3;
         ctx.save();
         ctx.translate(canvas.width / 2, canvas.height / 2);
-        ctx.rotate(i *Math.PI*8 / bufferLength);
+        ctx.rotate(i + Math.PI / bufferLength);
         const hue = i;
         ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%) ';
         ctx.fillRect(0, 0, barWidth, barHeight);
